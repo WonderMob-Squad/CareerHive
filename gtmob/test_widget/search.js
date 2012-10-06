b8dfeb0dd176999dde3f7fe3172cb39a40da7b00
Danny Cook <Danny.Cook@gatech.edu>
2012-10-06 18:31:49 -0400

    Added updated SSH code, for uniform testing environment.

---
+++ gtmob/test_widget/search.js
@@ -0,0 +1,45 @@
+function queryResult(){
+/*	$.ajax({
+		url: "/developer/wondermob/api/test_widget/simple/",
+		context: document.body,
+		success: function(data){
+			$('#major').html(data);
+		}
+	});*/
+
+ /*	$('<div>').simpledialog2({
+    	mode: 'blank',
+    	headerText: 'Loading...',
+    	headerClose: false,
+    	blankContent:"<div style='text-align:center'><br>Please wait...<br><br></div><img style='display:block;margin-left:auto;margin-right:auto' src='../gtmob/results_widget/images/spinner.gif' /><br>"
+  });
+*/
+	
+	
+	$.ajax({
+               type: "GET",
+               url: "/developer/wondermob/api/test_widget/simple/",
+               data: {
+			   'major': encodeURIComponent($('#major').val()),
+			   'degree_level': encodeURIComponent($('#degree_level').val()),
+			   'job_type': encodeURIComponent($('#job_type').val()),
+			   'work_auth': encodeURIComponent($('#work_auth').val())
+			   },
+               dataType: "json",
+               success: function(data) {
+               console.log(data);
+				 localStorage.setItem('companies', data);
+				 
+				 //Now, close the blocking popup:
+				 //$.mobile.sdCurrentDialog.close();
+				 	
+				 //Finally, force the navigation to the desired page:
+				// $.mobile.changePage(<insert path here>);
+           		}
+           	});
+}
+
+$(document).bind('mobileinit',function(){
+   $.mobile.selectmenu.prototype.options.nativeMenu = false;
+});
+
--- gtmob/test_widget/simple.php
+++ gtmob/test_widget/simple.php
@@ -1,7 +1,36 @@
 <?php
 	include 'db_helper.php';
-	
 
+	function getCompanies($major, $degree_level, $job_type, $work_auth) {
+		
+		$major = mysql_real_escape_string(urldecode($major));
+		$degree_level = mysql_real_escape_string(urldecode($degree_level));
+		$job_type = mysql_real_escape_string(urldecode($job_type));
+		$work_auth = mysql_real_escape_string(urldecode($work_auth));
+		
+		//$dbQuery = sprintf("select name from companies", mysql_real_escape_string($major));
+		$dbQuery = "SELECT DISTINCT companies.name
+					FROM companies, attendants_majors, majors, degree_levels, attendants_degree_levels, job_types, attendants_job_types, work_authorization_types, attendants_work_auth_levels
+					WHERE companies.id = attendants_majors.attendant_id
+					AND attendants_majors.major_id = majors.id
+					AND companies.id = attendants_job_types.attendant_id
+					AND attendants_job_types.job_type_id = job_types.id
+					AND companies.id = attendants_degree_levels.attendant_id
+					AND attendants_degree_levels.degree_levels_id = degree_levels.id
+					AND companies.id = attendants_work_auth_levels.attendant_id
+					AND attendants_work_auth_levels.work_auth_id = work_authorization_types.id
+					AND majors.name = '$major'
+					AND degree_levels.name = '$degree_level'
+					AND job_types.name = '$job_type'
+					AND work_authorization_types.name = '$work_auth'";
+
+		$result=getDBResultsArray($dbQuery);
+		header("Content-type: application/json");
+		//$result=''+$major+' '+$degree_level+' '+$job_type+' '+$work_auth;
+		echo json_encode($result);
+	}
+	
+/*
 	function getMajor() {
 		$dbQuery = sprintf("select name from majors");
 		$result=getDBResultsArray($dbQuery);
@@ -30,5 +59,5 @@
 		header("Content-type: application/json");
 		echo json_encode($result);
 	}
-	
+	*/
 	?>
