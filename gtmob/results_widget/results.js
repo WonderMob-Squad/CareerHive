7122772b34da2a8ee3728e25fdb0081b28c2d761
Danny Cook <Danny.Cook@gatech.edu>
2012-10-07 12:11:56 -0400

    Updated results_widget to dynamically generate company info pages.

--- gtmob/results_widget/results.js
+++ gtmob/results_widget/results.js
@@ -1,8 +1,9 @@
 console.log('RUNNING Results JAVASCRIPT');
 
 $('#results').bind('pageshow', function() {
-    			
+    			$('#companylist').children().remove('li');
     			var data = localStorage.getItem('companies');
+    			//alert(data);
     			data = JSON.parse(data);
     			
     			var elements = data.length;
@@ -10,10 +11,44 @@ $('#results').bind('pageshow', function() {
     			for (var i = 0; i < elements; i ++) {
     				if(data[i].name != null) {
     					//alert(data[i].name);
-    					$('#companylist').append("<li><a id=\"company" + i + "\" href=\"companyinfo.html\"><h3>" + data[i].name + "</h3><p>Location</p></li>");
+    					$('#companylist').append("<li id=\"" + i + "\"><a href=\"#companyinfo\"><h3>" + data[i].name + "</h3><p>Location</p></li>");
     				}
     			}
     			$('#companylist').listview("refresh");
     			
  
 });
+
+$('#companyinfo').bind('pageshow', function() {
+		var data = localStorage.getItem('info');
+		data = JSON.parse(data);
+		//alert(data);
+		//alert(data.name);
+		//
+		$('#littlename').text(data.name);
+		$('#majors').text(data.Majors);
+		$('#location').text(data.Booth);
+		$('#position').text(data.Job_Types);
+		$('#citizenship').text(data.Work_Auth);
+		$('#degree').text(data.Degree_Levels);
+
+
+
+});
+
+
+$('#companylist').delegate('li', 'vclick', function() {
+	var eventID = this.id;
+	//alert(this.id);
+	//alert(eventID);
+	var data = localStorage.getItem('companies');
+	data = JSON.parse(data);
+	data = data[eventID];
+	data = JSON.stringify(data);
+	//alert(data);
+	localStorage.setItem('info', data);
+	//var companyData = data[eventID].name;
+	
+	//alert(companyData);
+
+});  
